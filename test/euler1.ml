da05d09dabea778ef4e383c7ebe5760695402d84
Handiesto <klim.plyasov@gmail.com>
2023-02-22 11:38:05 +0300

    Test8

--- test/euler1.ml
+++ test/euler1.ml
@@ -6,29 +6,32 @@ open! OUnit2
 
 let test_nth_prime name expected n =
   name >:: (fun _ -> assert_equal expected (Recursion.nth_prime n))
-let test_nth_prime2 name expected n =
+
+let test_nth_prime_tailrec name expected n =
   name >:: (fun _ -> assert_equal expected (Tailrecursion.nth_prime n))
-let test_nth_prime3 name expected n =
+
+let test_nth_prime_map name expected n =
   name >:: (fun _ -> assert_equal expected (Map.nth_prime n))
 
 let tests =
   "test suite for nth_prime" >::: [
-    test_nth_prime "test 1" 80863 7915;
-    test_nth_prime "test 2" 4517 613;
-    test_nth_prime "test 3" 104743 10001;
-    test_nth_prime "test 4" 58567 5927;
-    test_nth_prime "test 5" 1794439 134677;
-(*   test_nth_prime2 "test 1" 80863 7915;
-    test_nth_prime2 "test 2" 4517 613;
-    test_nth_prime2 "test 3" 104743 10001;
-    test_nth_prime2 "test 4" 58567 5927;
-    test_nth_prime2 "test 5" 1794439 134677;
-    test_nth_prime3 "test 1" 80863 7915;
-    test_nth_prime3 "test 2" 4517 613;
-    test_nth_prime3 "test 3" 104743 10001;
-    test_nth_prime3 "test 4" 58567 5927;
-    test_nth_prime3 "test 5" 1794439 134677;*)
+    test_nth_prime "test 1 - Recursion" 80863 7915;
+    test_nth_prime "test 2 - Recursion" 4517 613;
+    test_nth_prime "test 3 - Recursion" 104743 10001;
+    test_nth_prime "test 4 - Recursion" 58567 5927;
+    test_nth_prime "test 5 - Recursion" 1794439 134677;
+    test_nth_prime_tailrec "test 1 - Tailrecursion" 80863 7915;
+    test_nth_prime_tailrec "test 2 - Tailrecursion" 4517 613;
+    test_nth_prime_tailrec "test 3 - Tailrecursion" 104743 10001;
+    test_nth_prime_tailrec "test 4 - Tailrecursion" 58567 5927;
+    test_nth_prime_tailrec "test 5 - Tailrecursion" 1794439 134677;
+    test_nth_prime_map "test 1 - Map" 80863 7915;
+    test_nth_prime_map "test 2 - Map" 4517 613;
+    test_nth_prime_map "test 3 - Map" 104743 10001;
+    test_nth_prime_map "test 4 - Map" 58567 5927;
+    test_nth_prime_map "test 5 - Map" 1794439 134677;
   ]
+
 
 let () =
   run_test_tt_main tests;
